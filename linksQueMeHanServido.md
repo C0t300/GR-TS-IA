19a50ea46f25d32fd0956e411e432fbc9ce015bf
c0t300 <cote.runin@gmail.com>
2021-12-04 17:36:06 -0300

    cambio de lista a vector
    como comentario, lo unico importante es cambiar insert a push_back

--- linksQueMeHanServido.md
+++ linksQueMeHanServido.md
@@ -4,3 +4,4 @@
 - https://stackoverflow.com/questions/27325964/splitting-char-array [string.split() pero en cpp]
 - https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c [string.split pero ahora con string en lugar de char*]
 - https://stackoverflow.com/questions/15362367/how-to-read-the-whole-lines-from-a-file-with-spaces [leer linea completa]
+- https://www.programiz.com/cpp-programming/vectors [con esto cambie facilito de lista a vector]
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list> 
+#include <vector> 
 #include <limits>
 #include <vector>
 using namespace std;
 
-void printLargeList(list<list<list<double>>> l){
+void printLargevector(vector<vector<vector<double>>> l){
     for (auto v : l){
         for (auto i : v){
             for (auto i2: i){
@@ -20,41 +20,39 @@ void printLargeList(list<list<list<double>>> l){
 // mejorqueseamenos / mejorqueseamas
 // minimizar
 
-list<list<double>> minCostPosibilities(list<list<list<double>>> airplanes){
+vector<vector<double>> minCostPosibilities(vector<vector<vector<double>>> airplanes){
     double min = std::numeric_limits<double>::infinity(); 
     for (auto airplane : airplanes){
-        list<double> data = airplane.front(); // Ei, Ti, Li, gi, hi
+        vector<double> data = airplane.front(); // Ei, Ti, Li, gi, hi
 
     }
 }
 
-double greedyTS(list<list<list<double>>> airplanes){
+double greedyTS(vector<vector<vector<double>>> airplanes){
     
     return 1;
 }
 
-list<string> split(string s, string delim){
-    list<string> retorno;
-    list<string>::iterator it = retorno.end();
+vector<string> split(string s, string delim){
+    vector<string> retorno;
     size_t pos = 0;
     std::string token;
     while ((pos = s.find(delim)) != std::string::npos) {
         token = s.substr(0, pos);
         if (! token.empty()){
-            retorno.insert(it, token);
+            retorno.push_back(token);
         }
         s.erase(0, pos + delim.length());
     }
     if (! s.empty()){
-        retorno.insert(it, s);
+        retorno.push_back(s);
     }
     return retorno;
 }
 
-list<list<list<double>>> openInstance(string nameFile){
+vector<vector<vector<double>>> openInstance(string nameFile){
     
-    list<list<list<double>>> retorno;
-    list<list<list<double>>>::iterator itR = retorno.end();
+    vector<vector<vector<double>>> retorno;
 
     // open a file in read mode.
     ifstream infile; 
@@ -65,31 +63,28 @@ list<list<list<double>>> openInstance(string nameFile){
     int i;
     
     for (i = 0; i < cantAviones; i++){
-        list<list<double>> bufPlane;
-        list<list<double>>::iterator itP = bufPlane.end();
-        list<double> buffer;
-        list<double>::iterator it = buffer.end();
+        vector<vector<double>> bufPlane;
+        vector<double> buffer;
         
         if (getline(infile,line)) {
-            list<string> splitted = split(line, " ");
+            vector<string> splitted = split(line, " ");
             
             for (auto num : splitted){
-                buffer.insert(it, stod(num));
+                buffer.push_back(stod(num));
             }
         }
-        bufPlane.insert(itP, buffer);
+        bufPlane.push_back(buffer);
 
-        list<double> buffer2;
-        list<double>::iterator it2 = buffer2.end();
+        vector<double> buffer2;
         if (getline(infile,line)) {
-            list<string> splitted = split(line, " ");
+            vector<string> splitted = split(line, " ");
 
             for (auto num : splitted){
-                buffer2.insert(it2, stod(num));
+                buffer2.push_back(stod(num));
             }
         }
-        bufPlane.insert(itP, buffer2);
-        retorno.insert(itR, bufPlane);
+        bufPlane.push_back(buffer2);
+        retorno.push_back(bufPlane);
     }
 
     // close the opened file.
@@ -101,8 +96,8 @@ list<list<list<double>>> openInstance(string nameFile){
 int main() {
 
     /*
-    La lista es basicamente una lista gigante de listas de datos de cada avion
-    La sublista de datos del avion se separa en dos:
+    La vectora es basicamente una vectora gigante de vectoras de datos de cada avion
+    La subvectora de datos del avion se separa en dos:
         La primera tiene Ei, Ti, Li, gi, hi. Es decir:
             early arrival
             ideal arrival
@@ -111,7 +106,7 @@ int main() {
             same pero despues de Ti
         La segunda tiene Sij, donde Sij es el tiempo de separacion entre el avion i y el j
     */
-    list<list<list<double>>> instance = openInstance("instancias/airland1.txt"); //[[[1,2],[3,4]], [[5,6],[7,8]]]
-    // printLargeList(instance);
+    vector<vector<vector<double>>> instance = openInstance("instancias/airland1.txt"); //[[[1,2],[3,4]], [[5,6],[7,8]]]
+    printLargevector(instance);
     return 0;
 }
